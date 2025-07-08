2e0a9aaf3e3edc5de9d126eff3eaf575d7a99666
Pascoooo <162371632+Pascoooo@users.noreply.github.com>
2025-07-08 11:07:51 +0200

    Inizializzazione repo per la Tesi

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(
+      home: CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          middle: Text('Firebase App'),
+        ),
+        child: Center(
+          child: Text('Hello Firebase!'),
+        ),
+      ),
+    );
+  }
+}
