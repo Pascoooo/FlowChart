150f86ddcea013e5db52626b7afc7928fac037d6
Pascoooo <162371632+Pascoooo@users.noreply.github.com>
2025-09-18 17:47:07 +0200

    Revert "rework file system bloc, flowchart bloc e dashboard page"
    This reverts commit 04026f2b9c0d23c4ee58d79e1192193e04393ef7.

--- lib/blocs/project_bloc/project_state.dart
+++ lib/blocs/project_bloc/project_state.dart
@@ -20,24 +20,16 @@ class ProjectLoading extends ProjectState {
   const ProjectLoading();
 }
 
-/// Stato che indica un errore bloccante.
+/// Stato che rappresenta un errore durante un'operazione
 class ProjectError extends ProjectState {
   final String message;
+
   const ProjectError({required this.message});
+
   @override
   List<Object> get props => [message];
 }
 
-/// Stato che indica la presenza di una sessione non salvata, per attivare il dialogo.
-class UnsavedChangesFound extends ProjectState {
-  final String projectId;
-  final String projectName;
-  const UnsavedChangesFound({required this.projectId, required this.projectName});
-  @override
-  List<Object> get props => [projectId, projectName];
-}
-
-/// Stato principale con i dati dei progetti caricati.
 class ProjectsLoaded extends ProjectState {
   final List<MyProject> projects;
   final MyProject? selectedProject;
@@ -48,6 +40,7 @@ class ProjectsLoaded extends ProjectState {
     this.selectedProject,
     this.error,
   });
+
 
   ProjectsLoaded copyWith({
     List<MyProject>? projects,
